73fa4ffe977901cd0803a748b03b3b61c8d0519a
javilonas <admin@lonasdigital.com>
2015-05-14 02:38:11 +0200

    Update files

--- ramdisk/init.post-boot.sh
+++ ramdisk/init.post-boot.sh
@@ -24,6 +24,31 @@ ln -s /system/xbin/busybox /system/bin/busybox
 sync
 
 # Asegurar Governor for Default
+
+chown system system /sys/devices/system/cpu/cpufreq/barry_allen/timer_rate
+chmod 0660 /sys/devices/system/cpu/cpufreq/barry_allen/timer_rate
+chown system system /sys/devices/system/cpu/cpufreq/barry_allen/timer_slack
+chmod 0660 /sys/devices/system/cpu/cpufreq/barry_allen/timer_slack
+chown system system /sys/devices/system/cpu/cpufreq/barry_allen/min_sample_time
+chmod 0660 /sys/devices/system/cpu/cpufreq/barry_allen/min_sample_time
+chown system system /sys/devices/system/cpu/cpufreq/barry_allen/hispeed_freq
+chmod 0660 /sys/devices/system/cpu/cpufreq/barry_allen/hispeed_freq
+chown system system /sys/devices/system/cpu/cpufreq/barry_allen/target_loads
+chmod 0660 /sys/devices/system/cpu/cpufreq/barry_allen/target_loads
+chown system system /sys/devices/system/cpu/cpufreq/barry_allen/go_hispeed_load
+chmod 0660 /sys/devices/system/cpu/cpufreq/barry_allen/go_hispeed_load
+chown system system /sys/devices/system/cpu/cpufreq/barry_allen/above_hispeed_delay
+chmod 0660 /sys/devices/system/cpu/cpufreq/barry_allen/above_hispeed_delay
+chown system system /sys/devices/system/cpu/cpufreq/barry_allen/boost
+chmod 0660 /sys/devices/system/cpu/cpufreq/barry_allen/boost
+chown system system /sys/devices/system/cpu/cpufreq/barry_allen/boostpulse
+chown system system /sys/devices/system/cpu/cpufreq/barry_allen/input_boost
+chmod 0660 /sys/devices/system/cpu/cpufreq/barry_allen/input_boost
+chown system system /sys/devices/system/cpu/cpufreq/barry_allen/boostpulse_duration
+chmod 0660 /sys/devices/system/cpu/cpufreq/barry_allen/boostpulse_duration
+chown system system /sys/devices/system/cpu/cpufreq/barry_allen/io_is_busy
+chmod 0660 /sys/devices/system/cpu/cpufreq/barry_allen/io_is_busy
+
 chmod 777 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo "barry_allen" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor # CPU0
 chmod -h 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
@@ -35,6 +60,19 @@ echo "barry_allen" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor # CPU
 chmod -h 0664 /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
 chmod 777 /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
 echo "barry_allen" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor # CPU3
+chmod -h 0664 /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+
+chmod 777 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor "barry_allen"
+chmod -h 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+chmod 777 /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+write /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor "barry_allen"
+chmod -h 0664 /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+chmod 777 /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+write /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor "barry_allen"
+chmod -h 0664 /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+chmod 777 /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+write /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor "barry_allen"
 chmod -h 0664 /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
 
 sync
